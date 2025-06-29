fde841bb93f9ae6e36635dcc7a7c8509e42b93e5
Aleksandr Ronzhin <alexandr.ronzhin@softgamings.com>
2025-06-29 12:29:55 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { Application } from "pixi.js";
 
 (async () => {
   const app = new Application();
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({
+    background: "#1099bb",
+    resizeTo: window
+  });
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 })();
