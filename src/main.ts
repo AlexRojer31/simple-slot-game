c151358255a2251a5c6bf89057688464ef13d54e
Aleksandr Ronzhin <alexandr.ronzhin@softgamings.com>
2025-07-01 10:40:22 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,21 @@ enum STATES {
     manifest.bundles.map((b: AssetsBundle) => b.name),
   );
 
+  const starCount = 30;
+  const graphics = new Graphics();
+  for (let index = 0; index < starCount; index++) {
+    const x = (index * 0.78695 * app.screen.width) % app.screen.width;
+    const y = (index * 0.9382 * app.screen.height) % app.screen.height;
+    const radius = 2 + Math.random() * 3;
+    const rotation = Math.random() * Math.PI * 2;
+
+    graphics
+      .star(x, y, 5, radius, 0, rotation)
+      .fill({ color: 0xffdf00, alpha: radius / 5 });
+  }
+
+  app.stage.addChild(graphics);
+
   const packman: Graphics = new Graphics()
     .arc(0, 0, 50, degInRad(30), degInRad(320))
     .stroke({
