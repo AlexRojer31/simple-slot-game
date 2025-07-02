7e1129a142834e209b5b9d9db7df46cb41942776
Alexandr Ronzhin <alexandrronzhin@yandex.ru>
2025-07-02 16:20:32 +0300

    upd

--- src/core/bandle-loader.ts
+++ src/core/bandle-loader.ts
@@ -2,12 +2,20 @@ import { Assets } from "pixi.js";
 import { LoadBandleEvent } from "./event-emitter/custom-events/load-bundle-event";
 import { Emitter } from "./event-emitter/event-emitter";
 import { BandleLoadedEvent } from "./event-emitter/custom-events/bandle-loaded-event";
+import { UnloadBandleEvent } from "./event-emitter/custom-events/unload-bundle-event";
 
 export function RunBandleLoader(): void {
   new BandleLoader();
 }
 
+enum BANDLE_STATES {
+  loaded = 0,
+  unloaded,
+}
+
 export class BandleLoader {
+  private loadedBandles: Map<string, BANDLE_STATES> = new Map();
+
   constructor() {
     this.subscribes();
   }
@@ -22,16 +30,34 @@ export class BandleLoader {
       });
     });
 
-    Emitter().addListener(LoadBandleEvent.NAME, (e: LoadBandleEvent) => {
+    Emitter().addListener(UnloadBandleEvent.NAME, (e: UnloadBandleEvent) => {
       this.unload(e.data.bandleName);
     });
   }
 
   private async load(bandleName: string): Promise<boolean> {
-    return await Assets.loadBundle(bandleName);
+    if (
+      this.loadedBandles.has(bandleName) &&
+      this.loadedBandles.get(bandleName) === BANDLE_STATES.loaded
+    ) {
+      return true;
+    }
+    const asset = await Assets.loadBundle(bandleName);
+    if (asset) {
+      this.loadedBandles.set(bandleName, BANDLE_STATES.loaded);
+    }
+
+    return false;
   }
 
   private async unload(bandleName: string): Promise<void> {
+    if (this.loadedBandles.get(bandleName) === BANDLE_STATES.unloaded) {
+      return;
+    }
+
+    if (this.loadedBandles.get(bandleName) === BANDLE_STATES.loaded) {
+      this.loadedBandles.set(bandleName, BANDLE_STATES.unloaded);
+    }
     return await Assets.unloadBundle(bandleName);
   }
 }
